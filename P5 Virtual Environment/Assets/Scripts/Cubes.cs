eee569c0241cebffae47426d709ffd0699c9c273
Kasper Sørensen <ksare17@student.aau.dk>
2019-10-22 12:09:33 +0200

    TileRoom
    MAde scripts for the tileroom

---
+++ P5 Virtual Environment/Assets/Scripts/Cubes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+
+    public bool good = false;
+    public GameObject tileController;
+    Vector3 originalPos;
+    public GameObject cube;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("/Map").GetComponent<TileRes>().tileState == false)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, 1, this.transform.position.z);
+        }
+    }
+
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.collider.name == "Player")
+        {
+            transform.position = new Vector3(transform.position.x, 0.9f, transform.position.z);
+            if (good == false)
+            {
+                GameObject.Find("/Map").GetComponent<TileRes>().tileState = false;
+            }
+
+            else
+            {
+                GameObject.Find("/Map").GetComponent<TileRes>().tileState = true;
+                print("Big booty bitches");
+            }
+        }
+
+    }
+
+
+
+}
