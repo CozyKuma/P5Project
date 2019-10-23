e711848b4fa639598b030525c4e2d58a1ff1f5b4
HamPaa35 <mfaldt17@student.aau.dk>
2019-10-23 12:18:59 +0200

    Light puzzle works with angles

--- P5 Virtual Environment/Assets/Scripts/Lighting puzzle/light_Position_check.cs
+++ P5 Virtual Environment/Assets/Scripts/Lighting puzzle/light_Position_check.cs
@@ -8,12 +8,12 @@ public class light_Position_check : MonoBehaviour
     
     public GameObject destinationLight;
     public float range;
-    public int coneAngle;
-    public int coneDiviation;
+    public float maxAngleDiviation;
     Vector3 destinationLightPosition;
-    private float maxAngleDiviation;
+    private Vector3 destinationLightRotation;
     private Vector3 rotationMax;
     private Vector3 rotationMin;
+    private bool solved = false;
 
 
     public Light thisLight;
@@ -22,38 +22,29 @@ public class light_Position_check : MonoBehaviour
     void Start()
     {
         destinationLightPosition = destinationLight.transform.position;
-
-        thisLight.spotAngle = coneAngle;
-        
-        maxAngleDiviation = coneAngle-coneDiviation;
-        
-        
-        Vector3 destinationLightRotation = destinationLight.transform.eulerAngles;
-
-        rotationMax = destinationLightRotation + new Vector3(maxAngleDiviation, maxAngleDiviation, maxAngleDiviation);
-        rotationMin = destinationLightRotation - new Vector3(maxAngleDiviation, maxAngleDiviation, maxAngleDiviation);
+        destinationLightRotation = destinationLight.transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentLight = transform.position;
-        
+        Vector3 playerLightPosition = transform.position;
+        Vector3 playerLightRotation = transform.eulerAngles;
         
 
-        if (Input.GetKeyDown("space"))
+        float angleDifX = 
+            Mathf.Abs(Mathf.DeltaAngle(playerLightRotation.x, destinationLightRotation.x));
+        float angleDifY =
+            Mathf.Abs(Mathf.DeltaAngle(playerLightRotation.y, destinationLightRotation.y));
+
+        if (angleDifX <= maxAngleDiviation && angleDifY <= maxAngleDiviation && Vector3.Distance(playerLightPosition, destinationLightPosition)<=range)
         {
-            print(transform.eulerAngles.ToString());
-            print(rotationMax);
-            print(rotationMin);
+            solved = true;
         }
-
-        if (transform.eulerAngles.x <= rotationMax.x && transform.eulerAngles.x >=rotationMin.x &&
-        transform.eulerAngles.y <= rotationMax.y && transform.eulerAngles.y >=rotationMin.y 
-        && Vector3.Distance(currentLight, destinationLightPosition)<=range) {
-            print("wooooo");
+        else
+        {
+            solved = false;
         }
-        
 
     }
 }
