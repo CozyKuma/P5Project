80554aea1cbf0ebe42a8f78b16838d94a50d0a07
Kasper Sørensen <ksare17@student.aau.dk>
2019-11-01 13:38:44 +0100

    Final
    Finalelele

--- P5 Virtual Environment/Assets/Scripts/CubesChild.cs
+++ P5 Virtual Environment/Assets/Scripts/CubesChild.cs
@@ -1,53 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class CubesChild : MonoBehaviour
 {
     public int tilePressed = 0;
-    public bool good;
-    public bool win;
-    public bool pressed;
+    public bool good, pressed;
     private float pressedPos = 0.9f;
     public GameObject tileController;
     Vector3 originalPos;
     public GameObject[] cubeArr;  
-        
-    
- 
-
-    void Start()
-    {
-           
-    }
-
     void Update()
     {
         if (GameObject.Find("/Map").GetComponent<TileRes>().tileState == false)
         {
-            this.transform.position = new Vector3(this.transform.position.x, 1, this.transform.position.z);
             pressed = false;
-
-
-
         }
-        
-
-
-        if (win == true)
-        {
-          //  print("Spørg sundhedsstyrelsen!");
-        }
-
-       // print("The tile is pressed: " + pressed);
     }
-
     public void OnTriggerEnter(Collider collision)
     {
         if (collision.name == "Player")
         {
-
-            print("Yeah baby!");
             gameObject.GetComponentInParent<Cubes>().pressed = true;
             Debug.Log("parent is "+gameObject.GetComponentInParent<Cubes>());
             if (good == false)
@@ -55,24 +25,11 @@ public class CubesChild : MonoBehaviour
                 GameObject.Find("/Map").GetComponent<TileRes>().tileState = false;
                 transform.parent.position = new Vector3(transform.position.x, pressedPos, transform.position.z);
             }
-
             if (good == true)
             {
-
                 GameObject.Find("/Map").GetComponent<TileRes>().tileState = true;
-              //  print("Spørg for helvede!");
                 transform.parent.position = new Vector3(transform.position.x, pressedPos, transform.position.z);
-
-
             }
-
         }
-
-    }
-
-    public bool getGood()
-    {
-        return good;
     }
 }
-
--- P5 Virtual Environment/Assets/Scripts/TileRes.cs
+++ P5 Virtual Environment/Assets/Scripts/TileRes.cs
@@ -1,29 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-
 public class TileRes : MonoBehaviour
 {
     public bool tileState;
-    
-    public int tilesX;
-    public int tilesZ;
+    public int tilesX, tilesZ;
     public GameObject tile = null;
     public List<GameObject> tileList = new List<GameObject>();
     private List<GameObject> goodTiles = new List<GameObject>();
     private List<GameObject> badTiles = new List<GameObject>();
     private List<GameObject> goodPressedTiles = new List<GameObject>();
-    
-    
-
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-
         foreach (GameObject obj in tileList)
         {
             Cubes tempCube = obj.GetComponent<Cubes>();
@@ -34,16 +21,12 @@ public class TileRes : MonoBehaviour
             }
         }
     }
-
-    // Update is called once per frame1
     void Update()
     {
         foreach (GameObject obj in goodTiles)
         {
             Cubes tempCube = obj.GetComponent<Cubes>();
             bool pressedFromChild = tempCube.pressed;
-            Debug.Log("bitch" + tempCube.good);
-            Debug.Log("bbb" + pressedFromChild);
             if (pressedFromChild && !goodPressedTiles.Contains(obj))
             {
                 print("Yeah Bitches!");
@@ -57,11 +40,8 @@ public class TileRes : MonoBehaviour
             win();
         }
     }
-
     void win()
     {
-        //Whatever needs to be done
-        print("Glerup er en cunt");
+        print("Ez win");
     }
-
 }
