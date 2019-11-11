1c3368b53922dfe0a84e89787ffb89410e4e06a7
Kasper Sørensen <ksare17@student.aau.dk>
2019-11-11 13:28:34 +0100

    Code optimized
    The code has been cleaned and optimized

--- P5 Virtual Environment/Assets/Scripts/CubesChild.cs
+++ P5 Virtual Environment/Assets/Scripts/CubesChild.cs
@@ -1,34 +1,34 @@
 ﻿using UnityEngine;
 public class CubesChild : MonoBehaviour
 {
-    public int tilePressed = 0;
     public bool good, pressed;
     private float pressedPos = 0.9f;
-    public GameObject tileController;
-    Vector3 originalPos;
-    public GameObject[] cubeArr;  
     void Update()
-    {
+    { //In case the player collides with an incorrect tile, this if-statement is true.
         if (GameObject.Find("/Map").GetComponent<TileRes>().tileState == false)
         {
+            //Resets the position of all tiles, so no tile is lowered.
+            transform.parent.position = new Vector3(transform.parent.position.x, 1, transform.parent.position.z);
             pressed = false;
         }
     }
+    //Checks if something (in this case, the player), collides with the collision trigger over the tiles
     public void OnTriggerEnter(Collider collision)
     {
         if (collision.name == "Player")
-        {
-            gameObject.GetComponentInParent<Cubes>().pressed = true;
-            Debug.Log("parent is "+gameObject.GetComponentInParent<Cubes>());
+        {//If a tile is pressed, the "pressed" boolean from "Cubes" class is set to true.
+            //gameObject.GetComponentInParent<CubesChild>().pressed = true;
+            pressed = true;
             if (good == false)
             {
+                //If the player hits a tile, where the "good" boolean is false, the "tilestate" is set to false, which activates line 8
                 GameObject.Find("/Map").GetComponent<TileRes>().tileState = false;
-                transform.parent.position = new Vector3(transform.position.x, pressedPos, transform.position.z);
             }
             if (good == true)
             {
+                //If the player hits a tile, where the "good" boolean is true, the "tilestate" is set to true and the tile is lowered
                 GameObject.Find("/Map").GetComponent<TileRes>().tileState = true;
-                transform.parent.position = new Vector3(transform.position.x, pressedPos, transform.position.z);
+                transform.parent.position = new Vector3(transform.parent.position.x, pressedPos, transform.parent.position.z);
             }
         }
     }
--- P5 Virtual Environment/Assets/Scripts/TileRes.cs
+++ P5 Virtual Environment/Assets/Scripts/TileRes.cs
@@ -1,47 +1,72 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+
 public class TileRes : MonoBehaviour
 {
     public bool tileState;
-    public int tilesX, tilesZ;
+
     public GameObject tile = null;
     public List<GameObject> tileList = new List<GameObject>();
     private List<GameObject> goodTiles = new List<GameObject>();
-    private List<GameObject> badTiles = new List<GameObject>();
+
     private List<GameObject> goodPressedTiles = new List<GameObject>();
     void Start()
-    {
+    {  //Loops through all tiles
         foreach (GameObject obj in tileList)
-        {
-            Cubes tempCube = obj.GetComponent<Cubes>();
+        {   //Checks each tile if they are supposed to be pressed, if they are, they are added to a list called "goodTiles"
+            CubesChild tempCube = obj.GetComponentInChildren<CubesChild>();
             bool goodFromChild = tempCube.good;
             if (goodFromChild)
             {
                 goodTiles.Add(obj);
             }
         }
+        
     }
-    void Update()
-    {
-        foreach (GameObject obj in goodTiles)
-        {
-            Cubes tempCube = obj.GetComponent<Cubes>();
-            bool pressedFromChild = tempCube.pressed;
-            if (pressedFromChild && !goodPressedTiles.Contains(obj))
-            {
-                print("Yeah Bitches!");
-                goodPressedTiles.Add(obj);
-            }
-        }
-        print("GoodPressedTiles " + goodPressedTiles);
-        print("GoodTiles " + goodTiles.Count);
-        if (goodTiles.Count == goodPressedTiles.Count)
-        {
-            win();
-        }
+    void FixedUpdate()
+    { 
+        checkGoodTilesPressed();
+        resetList();
+        winCondition();
+      //  print("GoodPressedTiles: " + goodPressedTiles.Count);
     }
     void win()
     {
         print("Ez win");
+    }
+
+    void checkGoodTilesPressed()
+    {
+        foreach (GameObject obj in goodTiles)
+        { //Checks each tile in the list if they are pressed. When pressed during the program, they are added to the list "goodPressedTiles"
+            CubesChild tempCube = obj.GetComponentInChildren<CubesChild>();
+            bool pressedFromChild = tempCube.pressed;
+            if (pressedFromChild && !goodPressedTiles.Contains(obj))
+            {
+                goodPressedTiles.Add(obj);
+            }
+        }
+    }
+
+    void resetList()
+    {
+        //If an incorrect tile is hit by the player, the goodPressedTiles list is cleared.
+        if (tileState == false)
+        { //Resets the "pressed" state to false before removing the objects from the "goodPressedTiles" list
+            foreach (GameObject obj in goodPressedTiles)
+            {
+                obj.GetComponentInChildren<CubesChild>().pressed = false;
+            }
+            //Clears the "goodPressedTiles" list
+            goodPressedTiles.Clear();
+        }
+    }
+
+    void winCondition()
+    {
+        if (goodTiles.Count == goodPressedTiles.Count)
+        {
+            win();
+        }
     }
 }
